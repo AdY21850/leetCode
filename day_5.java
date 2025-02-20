c1ef40caf9104feda4b37f80c628e9616901b506
AdY21850 <135639134+AdY21850@users.noreply.github.com>
2025-02-20 09:47:01 +0530

    majority element II(quest 229)

---
+++ day_5.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+      HashMap<Integer ,Integer> map=new HashMap<>();
+        int maxkey=0;
+        int maxfreq=nums.length/3;
+        for (int i=0;i<nums.length;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxfreq) {
+                maxkey = entry.getKey();
+                list.add(maxkey);
+            }
+        }
+        return list;  
+    }
+}
