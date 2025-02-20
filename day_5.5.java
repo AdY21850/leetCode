95ccf8942ee71d73fae8d46cab0ac28bef0a300c
AdY21850 <135639134+AdY21850@users.noreply.github.com>
2025-02-21 00:28:34 +0530

    Sort Colors(ques 75)

---
+++ day_5.5.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void sortColors(int[] nums) {
+int low=0;
+int high=nums.length-1;
+quickSort(nums,low,high);
+    }
+        static void quickSort(int arr[], int low, int high) {
+       if(low<high)
+       {
+           int pi=partition(arr,low,high);
+           quickSort(arr,low,pi-1);
+           quickSort(arr,pi+1,high);
+           
+       }
+    }
+
+    static int partition(int arr[], int low, int high) {
+        int pivot =arr[high];
+        int i=low-1;
+        for (int j=low;j<=high;j++)
+        {
+            if(arr[j]<pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        
+        return i+1;
+    }
+    
+}
